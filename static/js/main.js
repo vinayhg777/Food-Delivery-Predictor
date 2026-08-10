3d66871102f0e3377dadd85f09402fba444372d4
vinayhg777 <vinugouda2005@gmail.com>
2026-08-10 19:19:27 +0530

    removing spinner

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,7 @@
 (function ($) {
     "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
+ 
     
     
     // Initiate the wowjs
